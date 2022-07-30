d81a6bec9dc11d11f5d05075c668f2998011c17f
jual6332@colorado.edu <jual6332@colorado.edu>
2022-07-30 11:19:07 -0400

    Fixed syntax error

--- design/db.php
+++ design/db.php
@@ -3,7 +3,7 @@
 	$username = 'root';
 	$pass = '';
 	$dbname = 'biologyclassfinalproject';
-	$conn=mysqli_connect($servername,$username,$password,"$dbname");
+	$conn=mysqli_connect($servername,$username,$pass,"$dbname");
 	if (!$conn){
 		die('Could not Connect to MySQL Server:'.mysql_error());
 	}
